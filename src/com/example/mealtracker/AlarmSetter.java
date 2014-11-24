5fc55d970c97237805621294c7804695de5251e3
Jimmy <Jimmy090804@gmail.com>
2014-11-24 14:00:46 -0600

    Updataed alarm setter class

--- src/com/example/mealtracker/AlarmSetter.java
+++ src/com/example/mealtracker/AlarmSetter.java
@@ -1,5 +1,16 @@
 package com.example.mealtracker;
 
-public class AlarmSetter {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmSetter extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent service = new Intent(context, AlarmService.class);
+		service.setAction(AlarmService.CREATE);
+		context.startService(service);
+	}
 
 }
