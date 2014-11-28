66619a942b57c9a41f1dd8194908a8e67618a02d
JohnBorsch <johnborsch@hotmail.com>
2014-11-28 13:52:03 -0600

    Revert "Added changes to welcome file"
    This reverts commit e8e19a1e109e204d4a51778df87309552d46c469.

--- src/com/example/mealreminder/Welcome.java
+++ src/com/example/mealreminder/Welcome.java
@@ -1,13 +1,15 @@
 package com.example.mealreminder;
 
-import android.app.Activity;
+import com.example.mealreminder.R;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class Welcome extends Activity{
+public class Welcome extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
